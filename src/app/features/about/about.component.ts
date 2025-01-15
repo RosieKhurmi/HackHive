70a45484a3e4ab72a87a50729e021373faf8c372
ClaytonWas <claywasmund@gmail.com>
2025-01-15 13:18:19 -0500

    Working theme section before porting to main.

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { MenuComponent } from '../../core/menu/menu.component'; 
+import { Component, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MenuComponent } from '../../core/menu/menu.component';
+import { FooterComponent } from "../../core/footer/footer.component"; 
+import computerMan from '../../../assets/icons/undraw_firmware_3fxd.svg';
 
 @Component({
   selector: 'app-about',
-  imports: [MenuComponent],
+  imports: [MenuComponent, FooterComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  standalone: true,
+  styleUrl: './about.component.css',
+  encapsulation: ViewEncapsulation.None // ! This is necessary to apply styles to the dynamic SVGs
 })
 export class AboutComponent {
+  computerMan;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.computerMan = this.sanitizer.bypassSecurityTrustHtml(computerMan);
+  }
 }
