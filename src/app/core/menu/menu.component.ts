73288c148b7ffd0d6aea990530c219b3aa7327df
Hayden MacIntyre <haydenpmac@gmail.com>
2025-01-14 19:45:24 -0500

    feat: add dynamic SVG icons for social links and update SVG build config settings

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import linkedin from '../../../assets/icons/linkedin.svg';
+import instagram from '../../../assets/icons/instagram.svg';
+import x from '../../../assets/icons/x.svg';
+import email from '../../../assets/icons/email.svg';
 
 @Component({
   selector: 'app-menu',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   standalone: true,
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class MenuComponent {
-
   menuItems = [
     {
       label: 'Home',
@@ -30,4 +35,15 @@ export class MenuComponent {
     }
   ];
 
+  linkedin;
+  instagram;
+  x;
+  email;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.linkedin = this.sanitizer.bypassSecurityTrustHtml(linkedin);
+    this.instagram = this.sanitizer.bypassSecurityTrustHtml(instagram);
+    this.x = this.sanitizer.bypassSecurityTrustHtml(x);
+    this.email = this.sanitizer.bypassSecurityTrustHtml(email);
+  }
 }
