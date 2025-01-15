b9197c2eac10566b6346e4fcf2cee461a0df1d1b
RosieKhurmi <rosie.khurmi@ontariotechu.net>
2025-01-15 13:19:11 -0500

    style: add description for platinum sponsors

--- src/app/shared/sponsor-card/sponsor-card.component.ts
+++ src/app/shared/sponsor-card/sponsor-card.component.ts
@@ -1,13 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sponsor-card',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './sponsor-card.component.html',
   styleUrl: './sponsor-card.component.css'
 })
 export class SponsorCardComponent {
 
   image = input.required<{src: string; alt: string} >();
+  tier = input.required<string>();
+  @Input() description: string = "";
 
 }
