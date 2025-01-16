8445802c2e57af043c6af9be5202efe02a134ea7
RosieKhurmi <rosie.khurmi@ontariotechu.net>
2025-01-16 10:49:42 -0500

    refactor: changed position of code

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -3,7 +3,6 @@ import { MenuComponent } from '../../core/menu/menu.component';
 import { FooterComponent } from '../../core/footer/footer.component';
 import { HeroComponent } from '../../shared/hero/hero.component';
 import { ChallengesComponent } from '../../shared/challenges/challenges.component';
-import { ContactComponent } from '../../shared/contact/contact.component';
 
 @Component({
   selector: 'app-about',
@@ -12,7 +11,6 @@ import { ContactComponent } from '../../shared/contact/contact.component';
     FooterComponent,
     HeroComponent,
     ChallengesComponent,
-    ContactComponent
 ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MenuComponent } from '../../core/menu/menu.component';
 import { FooterComponent } from "../../core/footer/footer.component"; 
 import { ButtonComponent } from "../../core/button/button.component"; 
+import { ContactComponent } from '../../shared/contact/contact.component';
 import { ChallengesComponent } from '../../shared/challenges/challenges.component';
 import computerMan from '../../../assets/icons/undraw_firmware_3fxd.svg';
 
@@ -13,7 +14,8 @@ import computerMan from '../../../assets/icons/undraw_firmware_3fxd.svg';
     MenuComponent, 
     FooterComponent, 
     ButtonComponent,
-    ChallengesComponent
+    ChallengesComponent, 
+    ContactComponent
   ],
   templateUrl: './home.component.html',
   standalone: true,
