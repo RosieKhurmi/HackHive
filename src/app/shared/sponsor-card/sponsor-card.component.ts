3ad487882832cdecceae78fc44475bcb125870ad
RosieKhurmi <rosie.khurmi@ontariotechu.net>
2025-01-28 13:58:51 -0500

    style: dark

--- src/app/shared/sponsor-card/sponsor-card.component.ts
+++ src/app/shared/sponsor-card/sponsor-card.component.ts
@@ -14,6 +14,7 @@ export class SponsorCardComponent {
 
   image = input.required<{src: string; alt: string} >();
   tier = input.required<string>();
+  url = input.required<string>();
   @Input() description: string = "";
 
 }
